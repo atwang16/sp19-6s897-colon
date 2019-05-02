00bf68a353085f9dda4e15d0ad54ffcdaaf98143
Miguel Del Rio <drmiguel@mit.edu>
2019-05-01 21:35:38 -0400

    adding auc metric code

---
+++ roc_callback.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+from keras import backend as K
+
+def auc_metric(y_true, y_pred):
+    auc = tf.metrics.auc(y_true, y_pred)[1]
+    K.get_session().run(tf.local_variables_initializer())
+    return auc
