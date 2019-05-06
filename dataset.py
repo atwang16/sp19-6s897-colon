bbca508919189a7dee8dbb8cf5498dc3e2f54dd2
Elaine Xiao <elaine.y.xiao@gmail.com>
2019-05-06 14:03:56 -0400

    Updated data preprocessing

--- dataset.py
+++ dataset.py
@@ -1,43 +1,31 @@
 #!/usr/bin/env python3
 
 import sys, os
-from PIL import Image
 import numpy as np
 from os import listdir
 from os.path import isfile, join
 from keras.preprocessing import image
 
 
-def load_images_from_directory(directory, n_samples):
+def load_and_standardize_images(directory, n_samples):
 
 	image_arrays = []
 	files = [f for f in listdir(directory) if isfile(join(directory, f))]
 	if '.DS_Store' in files:
 		files.remove('.DS_Store')
 
-	i = 0
-	while True:
+	for i in range(n_samples):
 		img_path = files[i]
 		path = os.path.join(directory, img_path)
 		img = image.load_img(path, target_size=(224, 224)) # Resizing the images to (224, 224)
 		x = image.img_to_array(img)
-		yield x
-		i += 1
-
-
-def pixel_standardize(directory, n_samples):
-
-	image_generator = load_images_from_directory(directory, n_samples)
-	image_arrays=[]
-	for i in range(n_samples):
-		image = next(image_generator)
-		[r, g, b] = np.dsplit(np.array(image), np.array(image).shape[-1])
+		[r, g, b] = np.dsplit(np.array(x), np.array(x).shape[-1])
 		r = np.subtract(r, np.mean(r)) / np.std(r)
 		g = np.subtract(g, np.mean(g)) / np.std(g)
 		b = np.subtract(b, np.mean(b)) / np.std(b)
-		image = np.dstack((r, g, b))
-		image_arrays.append(image)
+		img = np.dstack((r, g, b))
+		image_arrays.append(img)
 
 	return np.array(image_arrays)
 
-trainX = pixel_standardize('/Users/exiao/MIT/hst.956_final_project/CVC-ClinicDB_converted/train/polyp', 612)
+trainX = load_and_standardize_images('/Users/exiao/MIT/hst.956_final_project/sp19-6s897-colon/CVC-ClinicDB_converted/train/polyp', 612)
